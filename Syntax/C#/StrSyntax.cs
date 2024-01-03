1f4ceac55d18c14bdd37beec53025759124c4049
lionelcopeland <104397929+lionelcopeland@users.noreply.github.com>
2024-01-03 16:52:47 -0600

    Create StrSyntax.cs

---
+++ Syntax/C#/StrSyntax.cs
@@ -0,0 +1,73 @@
+//SYNTAX Practice
+
+/* 
+//SAMPLE PROGRAM
+
+using System;  //
+
+namespace My_Awesome_Program
+{
+	class Program
+	{
+		static void Main(string[] args) //method called main "Entry Method".
+		{
+			Console.Title = "Skynet"; //Console is a class
+			Console.ForegroundColor = ConsoleColor.Green; //Changes text color
+			Console.WindowHeight = 40;
+			
+
+			
+			Console.Writeline("Hello, what's your name?"); //This is called an argument
+			
+			Console.ReadLine();
+			
+			Console.Writeline("My name is RX-9000.");
+			Console.Writeline("What is your favorite color?")
+			
+			Console.Readline();
+			
+			Console.WriteLine("Cool! Mine is destruction.")
+			
+			Console.ReadKey(); //Waits for user to press a button. Prevents window from closing
+		}
+	}
+} */
+
+// Exercise - Short Story Creator
+
+//Program allows user to tell short story based on answering questions.
+
+using System;
+
+namespace Short_Story
+{
+	static void Main(string[] args)
+	{
+		Console.Title = "Cyber World"
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WindowHeight = 40;
+			
+			//StoryLine
+			Console.Writeline("Once upon a time there was a cyborg named...");
+			
+			Console.Readline();
+			
+			Console.Writeline("...entered a burger king where he met a...");
+			
+			Console.ReadLine();
+			
+			Console.Writeline("What are you looking for, ye clunker?);
+			
+			Console.Readline();
+			
+			Console.WriteLine("...shouted the Cyborg!");
+			Console.Writeline("A raging Tesla burst through the walls of Burger King and crushed the cyborg.")
+			
+			
+			Console.ReadKey(;)
+			
+		}
+	}
+}
+
